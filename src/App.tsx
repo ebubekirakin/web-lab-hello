d7f320dfeb443dcf0f8e31d5086b5f43f6ddc18e
Ebubekir AKIN <ebubekirakin.dsc@gmail.com>
2026-02-17 11:29:15 +0300

    style: update page styling and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 function App() {
-return (
-<div >
-<h1 >Web Tasarimi ve Programlama </h1 >
-<h2 >LAB -1</h2 >
-<p>Ad Soyad: [Ebubekir AKIN]</p>
-<p>Ogrenci No: [230541132]</p>
-</div >
-)
+  return (
+    <div>
+      <h1>Ben Ebubekir AKIN </h1>
+      <p>Fırat Üniversitesi Yazılım Mühendisliği öğrencisiyim.</p>
+      
+      <h2>Hobilerim ve İlgi Alanlarım</h2>
+      <ul>
+        <li>Beşiktaş maçlarını takip etmek</li>
+        <li>Kafa dağıtmak için PES oynamak</li>
+        <li>Dizi izlemek</li>
+      </ul>
+    </div>
+  )
 }
- export default App
+
+export default App
