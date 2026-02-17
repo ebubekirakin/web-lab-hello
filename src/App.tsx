00f9a0d36a883d65cc7e58f3eb08364479288bc8
Ebubekir AKIN <ebubekirakin.dsc@gmail.com>
2026-02-17 11:21:10 +0300

    chore: initial project setup with Vite + React + TS

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+return (
+<div >
+<h1 >Web Tasarimi ve Programlama </h1 >
+<h2 >LAB -1</h2 >
+<p>Ad Soyad: [Ebubekir AKIN]</p>
+<p>Ogrenci No: [230541132]</p>
+</div >
+)
+}
+ export default App
